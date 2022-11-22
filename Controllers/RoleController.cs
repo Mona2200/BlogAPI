6ecf7278f3ea828c623076674836137721a0d556
Alyona Melekhova <alenamelehova13@gmail.com>
2022-11-22 23:21:04 +0700

    Добавлены методы для контроллера ролей.

--- Controllers/RoleController.cs
+++ Controllers/RoleController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using BlogProject.Data;
+using BlogProject.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
    public class RoleController : Controller
    {
-      public IActionResult Index()
+      private readonly IMapper _mapper;
+      private readonly UserService _userService = new UserService();
+      private readonly CommentService _commentService = new CommentService();
+      private readonly PostService _postService = new PostService();
+      private readonly RoleService _roleService = new RoleService();
+
+      public RoleController(IMapper mapper)
       {
-         return View();
+         _mapper = mapper;
+      }
+
+      [HttpGet]
+      [Route("GetRoles")]
+      public async Task<Role[]> GetRoles()
+      {
+         return await _roleService.GetRoles();
+      }
+
+      [HttpGet]
+      [Route("GetRoleByUserId")]
+      public async Task<Role[]> GetRoleByUserId(Guid userId)
+      {
+         return await _roleService.GetRoleByUserId(userId);
       }
    }
 }
