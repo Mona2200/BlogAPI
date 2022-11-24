e727673ae122e83de3d1071d9e2cf716c1fb772a
Alyona Melekhova <alenamelehova13@gmail.com>
2022-11-25 02:16:16 +0700

    Удалены ненужные методы в маппере.

--- MappingProfile.cs
+++ MappingProfile.cs
@@ -12,10 +12,6 @@ namespace API
          CreateMap<AddPostViewModel, Post>();
          CreateMap<AddCommentViewModel, Comment>();
          CreateMap<AddTagViewModel, Tag>();
-
-         CreateMap<User, RegisterViewModel>().ForMember(m => m.PasswordConfirm, opt => opt.MapFrom(u => u.Password));
-         CreateMap<Post, AddPostViewModel>();
-         CreateMap<Comment, AddCommentViewModel>();
       }
    }
 }
