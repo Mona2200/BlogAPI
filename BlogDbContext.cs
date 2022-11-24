6f880f968090c80bf9539a14fb79c7c49b247c51
Alyona Melekhova <alenamelehova13@gmail.com>
2022-11-25 02:43:41 +0700

    Добавлен контекст

---
+++ BlogDbContext.cs
@@ -0,0 +1,31 @@
+﻿using BlogProject.Models;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+
+namespace BlogProject.Data
+{
+   public class BlogDbContext : DbContext
+   {
+      public DbSet<Comment> Comment { get; set; } = null;
+      public DbSet<Post> Post { get; set; } = null;
+      public DbSet<Tag> Tag { get; set; } = null;
+      public DbSet<User> User { get; set; } = null;
+      public DbSet<TagPost> TagPost { get; set; } = null;
+      public DbSet<Role> Role { get; set; } = null;
+      public DbSet<UserRole> UserRole { get; set; } = null;
+
+      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+      {
+         var appDir = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+         var path = Path.Combine(appDir, "BlogProject/Data/blogDb.db");
+         var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = path };
+         var connectionString = connectionStringBuilder.ToString();
+         var connection = new SqliteConnection(connectionString);
+
+         optionsBuilder.UseSqlite(connection);
+      }
+   }
+}
